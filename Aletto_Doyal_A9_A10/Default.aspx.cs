27f0a3b735fb5547ee82dfd027a4b9100191bf69
Roy L Doyal <rldoyal@hotmail.com>
2018-11-11 11:08:00 -0700

    Repush of Roy's  Initial Commit...

--- Aletto_Doyal_A9_A10/Default.aspx.cs
+++ Aletto_Doyal_A9_A10/Default.aspx.cs
@@ -7,11 +7,59 @@ using System.Web.UI.WebControls;
 
 namespace Aletto_Doyal_A9_A10
 {
+
+    // Class for sessions objects
+    public enum accessType
+    {
+        Member, Staff
+    }
+    public class SessionObject
+    {
+
+        public SessionObject(string name, string hash, accessType access)
+        {
+            Name = name;
+            Hash = hash;
+            Access = access;
+
+        }
+        public SessionObject()
+        {
+            Name = null;
+            Hash = null;
+            Access = accessType.Member;
+        }
+        public string Name { get; set; }
+        public string Hash { get; set; }
+        public accessType Access { get; set; }
+    }
     public partial class _Default : Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblDebug.Text = Session.SessionID;
+        }
 
+        protected void brnStaff_Click(object sender, EventArgs e)
+        {
+            lblDebug.Text = Session.SessionID;
+            // Set the object stat to staff
+            SessionObject obj = (SessionObject)Session["User"];
+            obj.Access = accessType.Staff;
+            Session["User"] = obj;
+            Response.Redirect("Login.aspx");
+
+        }
+
+        protected void btnMember_Click(object sender, EventArgs e)
+        {
+            lblDebug.Text = Session.SessionID;
+            // Set the object stat to staff
+            SessionObject obj = (SessionObject)Session["User"];
+            obj.Access = accessType.Member;
+            Session["User"] = obj;
+            Response.Redirect("Login.aspx");
         }
     }
 }
--- Aletto_Doyal_A9_A10/Global.asax.cs
+++ Aletto_Doyal_A9_A10/Global.asax.cs
@@ -17,5 +17,25 @@ namespace Aletto_Doyal_A9_A10
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+
+        void Session_Start(object sender, EventArgs e)
+        {
+            string indexKey = "User";
+            SessionObject obj;
+            if (Session[indexKey] == null)
+            {
+                obj = new SessionObject();
+                Session[indexKey] = obj;
+            }
+        }
+
+        void Session_End(object sender, EventArgs e)
+        {
+            int count = (int)Session["Count"];
+            Session["Count"] = count - 1;
+
+
+        }
     }
 }
